02c4cb0a1722b1d7a8fd83f0ddf894f70c98e631
Le Tam <39722350+leductam2003@users.noreply.github.com>
2024-11-09 03:02:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const http = require('http');
-const axios = require('axios');
+const fs = require('fs').promises;
+const { createCanvas } = require('canvas');
+const https = require('https');
+
+async function getIPAddress() {
+  return new Promise((resolve, reject) => {
+    https.get('https://api.ipify.org?format=json', (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        const ip = JSON.parse(data).ip;
+        resolve(ip);
+      });
+    }).on('error', (err) => reject(err));
+  });
+}
+async function generateImage() {
+  const formattedDate = new Date().toUTCString();
+  const ip = await getIPAddress()
+
+  const width = 500;
+  const height = 500;
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext('2d');
+
+  // Background
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, width, height);
+
+  // Large Time Text
+  ctx.font = '150px VT323';
+  ctx.fillStyle = '#f5a991';
+  ctx.textAlign = 'center';
+  ctx.fillText("$IP", width / 2, 200);
+
+  // Smaller Date Text
+  ctx.font = '70px VT323';
+  ctx.fillStyle = '#00FF00';
+  ctx.fillText(ip, width / 2, 400);
+
+  // Return buffer
+  return canvas.toBuffer('image/png');
+}
 
 const server = http.createServer(async (req, res) => {
-  if (req.method === 'GET' && req.url === '/time') {
+  if (req.method === 'GET' && req.url === '/ip') {
     try {
-      const response = await axios.get("http://20.2.138.51:80/time", { responseType: 'arraybuffer' });
-      const imageBuffer = Buffer.from(response.data); // Create a buffer from the response data
-      res.writeHead(200, { 'Content-Type': 'image/png' }); // Set the appropriate content type
-      res.end(imageBuffer); // Send the image buffer as the response
+      const imageBuffer = await generateImage();
+
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Cache-Control': 'no-cache',
+      });
+      res.end(imageBuffer);
     } catch (error) {
       res.writeHead(500, { 'Content-Type': 'text/plain' });
       res.end('Internal Server Error');
@@ -20,5 +65,5 @@ const server = http.createServer(async (req, res) => {
 
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/holders`);
+  console.log(`Server is running on http://localhost:${port}/ip`);
 });
